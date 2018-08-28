398280d552600843d5ebc3e5877f29e459e50dfc
Zed83-83 <Zed7774gmail.com>
2018-08-28 16:02:38 +0300

    Initial Commit

---
+++ UITable View/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UITable View
+//
+//  Created by Zed Zezhenko on 28.08.2018.
+//  Copyright © 2018 Zed Zezhenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UITable View/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UITable View
+//
+//  Created by Zed Zezhenko on 28.08.2018.
+//  Copyright © 2018 Zed Zezhenko. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
